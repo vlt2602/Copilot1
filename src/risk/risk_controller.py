05dd56b26325ba05af2ec5dfcc4d2d4db3a990c8
vlt2602 <155764244+vlt2602@users.noreply.github.com>
2025-07-28 11:11:41 +0700

    Update risk_controller.py

--- src/risk/risk_controller.py
+++ src/risk/risk_controller.py
@@ -1,13 +1,26 @@
+import logging
 class RiskController:
     def __init__(self, config):
         self.config = config
         self.safe_mode = False
+        self.loss_count = 0
+        self.ai_error_count = 0
+        self.atr_spike = False
 
-    def check_risk(self, trade_result, ai_status, market_volatility):
-        # TODO: kiểm tra các điều kiện kick SafeMode
-        if ai_status == 'error' or trade_result == 'loss' or market_volatility == 'high':
+    def check_safe_mode(self, trade_results, ai_status, atr):
+        if ai_status == 'error':
+            self.ai_error_count += 1
+        else:
+            self.ai_error_count = 0
+        if trade_results[-3:] == ['loss','loss','loss']:
+            self.loss_count = 3
+        else:
+            self.loss_count = 0
+        if atr > self.config.get('atr_threshold', 0):
+            self.atr_spike = True
+        if self.ai_error_count >= 2 or self.loss_count >= 3 or self.atr_spike:
             self.safe_mode = True
-            print("SafeMode enabled!")
+            logging.warning('SafeMode enabled!')
         else:
             self.safe_mode = False
         return self.safe_mode
