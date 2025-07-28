d1fd1309237ffc1bff2132f05846cc9266e6b21b
vlt2602 <155764244+vlt2602@users.noreply.github.com>
2025-07-28 11:13:49 +0700

    Update log_manager.py

--- src/utils/log_manager.py
+++ src/utils/log_manager.py
@@ -1,4 +1,13 @@
+import logging, os
+
 class LogManager:
-    def log(self, module, msg):
-        print(f"[{module}] {msg}")
-        # TODO: Lưu log ra file hoặc database
+    def __init__(self, log_file='logs/thopper.log'):
+        os.makedirs(os.path.dirname(log_file), exist_ok=True)
+        logging.basicConfig(filename=log_file, level=logging.INFO,
+                            format='%(asctime)s %(levelname)s %(module)s: %(message)s')
+    def info(self, module, msg):
+        logging.info(f'[{module}] {msg}')
+    def warning(self, module, msg):
+        logging.warning(f'[{module}] {msg}')
+    def error(self, module, msg):
+        logging.error(f'[{module}] {msg}')
